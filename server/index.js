c0b4b0ad4236a328a73b2633490644c8d6448442
Redrezo11 <Redrezo@users.noreply.github.com>
2026-01-22 19:56:50 +0300

    Fix Express 5 wildcard route syntax

--- server/index.js
+++ server/index.js
@@ -344,7 +344,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
 
   // Handle React routing, return all requests to React app
-  app.get('*', (req, res) => {
+  app.get('/{*splat}', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist', 'index.html'));
   });
 }
